f36252daab835ebbfa721d79197aaf48229a0585
asanchez8801 <jasanchez8801@gmail.com>
2024-05-22 20:10:06 -0500

    Express

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 "use strict";
-
-console.log("works Albeiro!");
+ const express = require("express");
+ 
+ const servidor = express();
+ 
+ servidor.get("/", (solicitud , respuesta)=>{
+  respuesta.json({
+  saludo: "Hola Albeiro"
+  })
+ });
+  
+ servidor.listen(3000);
+ console.log("corriendo servidor");

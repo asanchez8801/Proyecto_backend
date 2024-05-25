ff5137f867e61d83331599e65b41243ca3790e62
asanchez8801 <jasanchez8801@gmail.com>
2024-05-24 22:03:37 -0500

    Actualizacion final postman

--- src/controladores/controladorUsuarios.js
+++ src/controladores/controladorUsuarios.js
@@ -7,42 +7,68 @@ const ControladorUsuarios = {
     if (solicitud.body.nombre === "") throw new Error("Faltan el campo Nombres")
     if (solicitud.body.apellidos === "") throw new Error("Faltan el campo Apellidos")
     if (solicitud.correo === "") throw new Error("Faltan el campo Correo")
-    if (solicitud.body.contrasena === "") throw new Error("Faltan el campo contraseña")
+    if (solicitud.body.contrasena === "") throw new Error("Faltan el campo Contraseña")
     if (solicitud.body.confirmarContrasena === "") throw new Error("Faltan el campo confirmar Contraseña")
-    if (solicitud.body.edad === "") throw new Error("Faltan el campo edad")
-    if (solicitud.body.telefono === "") throw new Error("Faltan el campo telefono")
-    if (solicitud.body.nomContactoEmergencia === "") throw new Error("Faltan el campo Nombre contacto emergencia")
-    if (solicitud.body.numContactoEmergencia === "") throw new Error("Faltan el campo Numero contacto emergencia")
-    if (solicitud.body.parentezcoContacto === "") throw new Error("Faltan el campo parentezco contacto emergencia")
-    if (solicitud.body.direccion === "") throw new Error("Faltan el campo dirección")
+    if (solicitud.body.edad === "") throw new Error("Faltan el campo Edad")
+    if (solicitud.body.telefono === "") throw new Error("Faltan el campo Teléfono")
+    if (solicitud.body.nomContactoEmergencia === "") throw new Error("Faltan el campo Nombre Contacto Emergencia")
+    if (solicitud.body.numContactoEmergencia === "") throw new Error("Faltan el campo Número Contacto Emergencia")
+    if (solicitud.body.parentezcoContacto === "") throw new Error("Faltan el campo parentezco Contacto Emergencia")
+    if (solicitud.body.direccion === "") throw new Error("Faltan el campo Dirección")
     if (solicitud.body.fechaNacimiento === "") throw new Error("Faltan el campo fecha de Nacimiento")
-    if (solicitud.body.genero === "") throw new Error("Faltan el campo género")
-    if (solicitud.body.pais === "") throw new Error("Faltan el campo país")
-    if (solicitud.body.ciudad === "") throw new Error("Faltan el campo ciudad")
+    if (solicitud.body.genero === "") throw new Error("Faltan el campo Género")
+    if (solicitud.body.pais === "") throw new Error("Faltan el campo País")
+    if (solicitud.body.ciudad === "") throw new Error("Faltan el campo Ciudad")
     if (solicitud.body.nivEscolar === "") throw new Error("Faltan el campo Nivel Escolar")
-    if (solicitud.body.profesion === "") throw new Error("Faltan el campo profesión")
-                                                                        
-    respuesta.json({mensaje: "POST Usuario Works!"})
-        
+    if (solicitud.body.profesion === "") throw new Error("Faltan el campo Profesión")
+                                                                     
+    respuesta.json({mensaje: "Crear Usuario Works!"})   
     } catch (error) {
-        respuesta.json({error: true, mensaje: "Ocurrio un error al crear usuario"})
+        respuesta.json({error: true, mensaje: "Ocurrió un error al crear usuario"})
         console.log(error);
     }},
-       
-       leerUsuario: (solicitud , respuesta)=>{
-        respuesta.json({mensaje: "GET Usuario Works!"})
+    
+    /* ----------------------------------------------------- */ 
+       leerUsuario: async (solicitud , respuesta)=>{
+       try {
+        console.log(solicitud.params.id)
+        respuesta.json({mensaje: "leer Usuario Works!"})
+       } catch (error) {
+        respuesta.json({error: true, mensaje: "Ocurrió un error al leer usuario"})
+        console.log(error);
+       }
        },
        
-       leerUsuarios: (solicitud , respuesta)=>{
-        respuesta.json({mensaje: "GET Usuarios Works!"})
+    /* ----------------------------------------------------- */ 
+       leerUsuarios: async (solicitud , respuesta)=>{
+       try {
+        respuesta.json({mensaje: "Leer todos los Usuarios Works!"})
+       } catch (error) {
+        respuesta.json({error: true, mensaje: "Ocurrió un error al leer todos los usuarios"})
+        console.log(error);
+       }
        },
-       
-       actualizarUsuario: (solicitud , respuesta)=>{
-        respuesta.json({mensaje: "PUT Usuario Works!"})
+    /* ----------------------------------------------------- */    
+       actualizarUsuario: async (solicitud , respuesta)=>{
+        try {
+            console.log(solicitud.params.id)
+            console.log("solicitud: ", solicitud.body);
+        
+            respuesta.json({mensaje: "Actualizar Usuario Works!"});
+        } catch  {
+            respuesta.json({error: true, mensaje: "Ocurrió un error al actualizar el usuario"})
+        console.log(error);
+        }
        },
-       
-       eliminarUsuario: (solicitud , respuesta)=>{
-        respuesta.json({mensaje: "DELETE Usuario Works!"})
+    /* ----------------------------------------------------- */    
+       eliminarUsuario: async (solicitud , respuesta)=>{
+         try {
+        console.log(solicitud.params.id)
+        respuesta.json({mensaje: "Eliminar Usuario Works!"})
+       } catch (error) {
+        respuesta.json({error: true, mensaje: "Ocurrió un error al eliminar el usuario"})
+        console.log(error);
+       }
        },
 }
 
